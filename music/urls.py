6e998796e505b90662880b6652cbacd453503248
ounamg <ounamg008@gmail.com>
2018-06-12 01:00:49 +0530

    Initial commit

---
+++ music/urls.py
@@ -0,0 +1,7 @@
+#music;;
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index)
+]
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\user\PycharmProjects\fsp\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
